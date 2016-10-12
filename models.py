c32a61a28ec170f254b25c249a6de6772b3b7683
Kris <kdwarn@gmail.com>
2016-10-12 16:05:45 +0000

    fix misspelling of sources in foreign key ref

--- models.py
+++ models.py
@@ -59,7 +59,7 @@ class Tokens(db.Model, UserMixin):
 class Documents(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id', onupdate="CASCADE", ondelete="CASCADE"))
-    source_id = db.Column(db.Integer, db.ForeignKey('source.id'))
+    source_id = db.Column(db.Integer, db.ForeignKey('sources.id'))
     title = db.Column(db.String(300))
     link = db.Column(db.String(300))
     created = db.Column(db.DateTime)
